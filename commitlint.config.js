e2ac3b1aa2a6cfc5c5d438de6c8567d4a176ad11
t303291556 <303291556@qq.com>
2022-06-30 14:12:28 +0800

    feat: 登录页

--- commitlint.config.js
+++ commitlint.config.js
@@ -24,4 +24,3 @@ module.exports = {
     'subject-case': [0]
   }
 }
-
---
+++ src/views/login/indexWeb.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="login-container">
+    <el-form class="login-form">
+      <div class="title-container">
+        <h3 class="title">永恒登陆</h3>
+      </div>
+      <el-form-item>
+        <span class="svg-container">
+          <el-icon>
+            <avatar />
+          </el-icon>
+        </span>
+        <el-input placeholder="username" name="username" type="text" />
+      </el-form-item>
+      <!-- password  -->
+      <el-form-item>
+        <span class="svg-container">
+          <el-icon>
+            <avatar />
+          </el-icon>
+        </span>
+        <el-input placeholder="password" name="password" />
+        <span class="show-pwd">
+          <el-icon>
+            <avatar />
+          </el-icon>
+        </span>
+      </el-form-item>
+      <!-- 登陆按钮 -->
+      <el-button type="primary" style="width: 100%; margin-bottom: 30px"
+        >登陆</el-button
+      >
+    </el-form>
+  </div>
+</template>
+<script setup>
+import { Avatar } from '@element-plus/icons'
+import {} from 'vue'
+</script>
+<style lang="scss" scoped></style>
