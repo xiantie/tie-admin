dccd84ef3f1465ff7332ad5e8a4241800d6535b7
t303291556 <303291556@qq.com>
2022-06-30 21:16:43 +0800

    feat: svg图标

--- src/views/login/indexWeb.vue
+++ src/views/login/indexWeb.vue
@@ -2,22 +2,17 @@
   <div class="login-container">
     <el-form class="login-form">
       <div class="title-container">
-        <h3 class="title">永恒登陆</h3>
+        <h3 class="title">用户登陆</h3>
       </div>
+      <!-- username -->
       <el-form-item>
-        <span class="svg-container">
-          <el-icon>
-            <avatar />
-          </el-icon>
-        </span>
+        <span class="svg-container"></span>
         <el-input placeholder="username" name="username" type="text" />
       </el-form-item>
       <!-- password  -->
       <el-form-item>
         <span class="svg-container">
-          <el-icon>
-            <avatar />
-          </el-icon>
+          <svg-icon icon="https://res.lgdsunday.club/user.svg" />
         </span>
         <el-input placeholder="password" name="password" />
         <span class="show-pwd">
@@ -35,6 +30,7 @@
 </template>
 <script setup>
 import { Avatar } from '@element-plus/icons'
+import SvgIcon from '@/components/SvgIcon/SvgIconIndex'
 import {} from 'vue'
 </script>
 <style lang="scss" scoped>
@@ -57,17 +53,18 @@ $cursor: #fff;
     margin: 0 auto;
     overflow: hidden;
 
-    ::v-deep .el-form-item {
+    :deep(.el-form-item) {
       border: 1px solid rgba(255, 255, 255, 0.1);
       background: rgba(0, 0, 0, 0.1);
       border-radius: 5px;
       color: #454545;
     }
 
-    ::v-deep .el-input {
-      display: inline-block;
+    :deep(.el-input) {
+      // display: inline-block;
       height: 47px;
       width: 85%;
+      // --el-input-bg-color:transparent;
 
       input {
         background: transparent;
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  chainWebpack(config) {
+    config.module
+      .rule('element-plus-2')
+      .test(/\.mjs/)
+      .type('javascript/auto')
+      .include.add(/node_modules/)
+      .end()
+  }
 })
