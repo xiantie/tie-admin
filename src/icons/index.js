944efae9e2c72cc00bf024a32afea1973ea276b3
t303291556 <303291556@qq.com>
2022-06-30 21:34:37 +0800

    feat: 外部svg

---
+++ src/icons/index.js
@@ -0,0 +1,10 @@
+// 1. 导入所有svg图标
+//2. 完成SvgIcon的全局注册
+
+const svgRequire = require.context('./svg', false, /\.svg$/)
+
+//此时返回了一个Require函数，可以接收一个request的参数，用于require的导入
+//该函数提供了三个属性，可以通过svgRequire.keys()获得所有的svg图标
+//遍历图标，把图标作为request参数导入到svgRequire导入函数中，完成本地svg图标
+console.log(svgRequire.keys())
+svgRequire.keys().forEach((svgIcon) => svgRequire(svgIcon))
--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import installElementPlus from './plugins/element'
-import './styles/index.scss'
+// 初始化样式表
+import '@/styles/index.scss'
+//导入svgIcon
+import '@/icons'
 
 const app = createApp(App)
 installElementPlus(app)
