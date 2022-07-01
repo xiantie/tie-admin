45f06297cf8664dd45ce82f9aa11b623aac3c2be
t303291556 <303291556@qq.com>
2022-07-01 10:59:58 +0800

    feat: layout

--- src/layout/indexLayout.vue
+++ src/layout/indexLayout.vue
@@ -6,4 +6,22 @@
 import {} from 'vue'
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import '~@/styles/mixin.scss';
+@import '~@/styles/variables.scss';
+
+.app-wrapper {
+  @include clearfix;
+  position: relative;
+  height: 100%;
+  width: 100%;
+}
+
+.fixed-header {
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 9;
+  width: calc(100% - #{$sideBarWidth});
+}
+</style>
