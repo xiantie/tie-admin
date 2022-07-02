868af967acab94377db8909254e99566008b10d1
t303291556 <303291556@qq.com>
2022-07-02 17:30:42 +0800

    feat: 国际化完善

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -3,6 +3,13 @@ import mEnLocale from './lang/en'
 
 import store from '@/store'
 import { createI18n } from 'vue-i18n'
+/**
+ * 返回当前 lang
+ */
+function getLanguage() {
+  return store && store.getters && store.getters.language
+}
+
 const messages = {
   en: {
     msg: {
@@ -17,13 +24,12 @@ const messages = {
 }
 
 // console.log(store.getters)
-const locale = store.getters.language
 const i18n = createI18n({
   // 使用 Composition API 模式，则需要将其设置为false
   legacy: false,
   // 全局注入 $t 函数
   globalInjection: true,
-  locale,
+  locale: getLanguage(),
   messages
 })
 
